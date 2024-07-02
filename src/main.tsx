705cd667286dc8ad2643cc0c83867bbfff7ec695
oorjagandhi <oorjagandhi@gmail.com>
2024-07-02 12:20:27 +1200

    Added import statement

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import React from 'react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <App />
