f559ac4a4e95a8f4e233ffa5ec14bade92aced2b
oorjagandhi <oorjagandhi@gmail.com>
2024-07-04 20:20:27 +1200

    strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,10 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { StrictMode } from 'react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <App />
+    <StrictMode>
+        <App />
+    </StrictMode>
 )
