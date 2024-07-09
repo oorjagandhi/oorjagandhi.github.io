b6cb6ee62b92283e4c9a905e775f90b69d440548
oorjagandhi <oorjagandhi@gmail.com>
2024-07-09 21:41:19 +1200

    Made website responsive

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -3,34 +3,29 @@ import Fade from "react-reveal/Fade";
 import TriangleIcon from "./TriangleIcon";
 
 const AboutMe: React.FC = () => {
-  const technologies = [
-    "Java",
-    "React.js",
-    "C",
-    "Typescript"
-  ];
+  const technologies = ["Java", "React.js", "C", "Typescript"];
 
   return (
     <Fade bottom duration={1000}>
-      <div className="flex items-center justify-center mt-40 mb-100">
-        <section className="flex flex-col lg:flex-row justify-center items-center bg-background w-[1000px] text-whiteish font-display">
-          <div className="lg:w-[1000px] md:w-[650px] w-[500px] px-10">
+      <div className="flex items-center justify-center mt-20 mb-20 px-4">
+        <section className="flex flex-col lg:flex-row justify-center items-center bg-background w-[500px] md:w-[650px] lg:w-[1000px] text-whiteish font-display p-6 lg:p-10">
+          <div>
             <div className="flex items-center mb-6">
-              <h2 className="text-lightpurple text-[40px] font-bold">
+              <h2 className="text-lightpurple text-3xl sm:text-4xl font-bold">
                 / about me
               </h2>
-              <div className="h-[1px] bg-darkpurple ml-4 w-36 lg:w-80 mt-4"></div>
+              <div className="h-1 bg-darkpurple ml-4 w-24 md:w-80 lg:w-80 mt-2"></div>
             </div>
-            <p className="lg:text-[18px] text-lesswhite font-medium">
+            <p className="text-sm sm:text-base md:text-base lg:text-lg text-lesswhite font-light">
               I am currently in my second year studying towards a Bachelor of
               Engineering, specialising in <b>Software Engineering</b> at the
               University of Auckland.
             </p>
-            <p className="lg:text-[18px] text-lesswhite font-medium mt-3 mb-3">
+            <p className="text-sm sm:text-base md:text-base lg:text-lg text-lesswhite font-light mt-3 mb-3">
               Here are some technologies I have been working with:
             </p>
 
-            <ul className="grid grid-cols-2 gap-2 mb-4 text-[18px] text-lesswhite font-medium">
+            <ul className="grid grid-cols-2 gap-2 mb-4 text-sm sm:text-base md:text-base lg:text-lg text-lesswhite font-light">
               {technologies.map((tech, index) => (
                 <Fade bottom duration={1000} delay={index * 200} key={index}>
                   <li className="flex items-center">
@@ -40,11 +35,11 @@ const AboutMe: React.FC = () => {
                 </Fade>
               ))}
             </ul>
-            <p className="lg:text-[18px] text-lesswhite font-medium">
+            <p className="text-sm sm:text-base md:text-base lg:text-lg text-lesswhite font-light">
               Outside of studying, I love trying out new cafes and spending time with my friends and family. I also like building legos.
             </p>
           </div>
-          <div className="lg:w-1/3 flex justify-center lg:justify-end lg:mt-24 mt-10">
+          <div className="lg:w-1/3 flex justify-center lg:justify-end lg:mt-20 m-5">
             <img
               src="/profilepic.JPG"
               alt="Profile"
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -9,13 +9,7 @@ const Projects: React.FC = () => {
       title: "SESA Website",
       description:
         "Contributed to the redevelopment of the SESA (UoA's Software Engineering Student Association) website. This project redesigned the current website to improve user experience and provide a more modern and responsive design.",
-      technologies: [
-        "Node.js",
-        "React.js",
-        "Typescript",
-        "HTML",
-        "TailwindCSS",
-      ],
+      technologies: ["Node.js", "React.js", "Typescript", "HTML", "TailwindCSS"],
       github: "https://github.com/sesauoa/sesa.org.nz",
       link: "https://sesa.org.nz",
     },
@@ -24,57 +18,50 @@ const Projects: React.FC = () => {
       description:
         "An application that displays real-time weather updates and forecasts across multiple locations, including key features such as a location search and current weather conditions display (temperature, humidity, wind speed).",
       technologies: ["Java", "Swing", "Weather Forecast API", "Geocoding API"],
-      github: "https://github.com",
+      github: "https://github.com/oorjagandhi/Weather-App",
     },
     {
       title: "Routing Application",
       description:
         "A routing application that uses Breadth First Search (BFS) and Graph data structures to optimise global delivery routes. The system calculates the shortest path and cross-border taxes between countries, ensuring efficient and cost-effective routing decisions.",
       technologies: ["Java"],
-      github: "https://github.com",
+      github: "https://github.com/oorjagandhi/Routing-Application",
     },
     {
-      title: "Odds and Evens",
+      title: "Odds and Evens Game",
       description:
         "An odds and evens game that allows users to play against the computer. The game includes features like a variable AI strategy that adapts based on the game progress.",
       technologies: ["Java"],
-      github: "https://github.com",
+      github: "https://github.com/oorjagandhi/Odds-and-Evens-Game",
     },
     {
       title: "Venue Booking System",
       description:
         "A venue hire system developed using object-oriented principles, featuring terminal-based management of bookings, venues, and services.",
       technologies: ["Java"],
-      github: "https://github.com",
+      github: "https://github.com/oorjagandhi/Venue-Hire-System",
     },
     {
       title: "Battleships Game",
       description:
         "A customisable Battleships game that allows users to play against the computer. The game includes features such as customisable board sizes, ship placements, and a hit/miss tracking system.",
       technologies: ["C"],
-      github: "https://github.com",
+      github: "https://github.com/oorjagandhi/Battleships-Game",
     },
-    // {
-    //   title: "Image Fingerprinting",
-    //   description:
-    //     "A program that uses  image hashing techniques to create unique identifiers for efficient similarity checks and rapid identification of image originals from thumbnails.",
-    //   technologies: ["MATLAB"],
-    //   github: "https://github.com",
-    // },
   ];
 
   return (
     <Fade bottom duration={1000}>
-      <div className="flex items-center justify-center mt-40">
-        <section className="flex flex-col justify-center items-center bg-background lg:w-[1000px] md:w-[650px] w-[500px] text-whiteish font-display mb-20">
-          <div className="px-10 w-full">
+      <div className="flex items-center justify-center mt-20 mb-20 px-4">
+        <section className="flex flex-col justify-center items-center bg-background w-[500px] md:w-[650px] lg:w-[1000px] text-whiteish font-display p-6 lg:p-10">
+          <div className="w-full">
             <div className="flex items-center mb-6">
-              <h2 className="text-lightpurple text-[40px] font-bold">
+              <h2 className="text-lightpurple text-3xl sm:text-4xl font-bold">
                 / projects
               </h2>
-              <div className="h-[1px] bg-darkpurple ml-4 w-36 lg:w-80 mt-4"></div>
+              <div className="h-1 bg-darkpurple ml-4 w-24 md:w-80 lg:w-80 mt-2"></div>
             </div>
-            <div className="w-full grid md:grid-cols-2 lg:grid-cols-3 gap-4 justify-center">
+            <div className="w-full grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 justify-center">
               {projectData.map((project, index) => (
                 <Fade bottom duration={1000} distance="100px" delay={index * 300} key={index}>
                   <div className="bg-cardbg p-6 rounded-lg shadow-lg md:w-[280px] lg:w-[300px] lg:h-[350px] flex flex-col justify-between hover:bg-cardhover transition-all duration-300 ease-in-out">
